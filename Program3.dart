89bf74ed664cc602a594406d43663abcc2bde48b
chaitanyaph <chaitanyaphalke28@gmail.com>
2024-01-17 13:28:58 +0530

    One more file added

---
+++ Program3.dart
@@ -0,0 +1,21 @@
+class Parent{
+	
+	Parent(){
+
+		print("In Parent Constructor");
+	}
+}
+
+class Child extends Parent{
+	
+	Child(){
+
+		print("In Child Constructor");
+	}
+}
+
+void main(){
+	
+	Child obj = new Child();
+	
+}
